76cf0fed8ef8a041e5d6e9ec0425ac4e7c6d5185
Lingxi <thelastoreointhejar@gmail.com>
2023-08-10 03:34:24 +0800

    feat: Added treesitter for Java

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "python",
+            "html",
+            "css",
+            "javascript",
+            "typescript",
+            "go",
+            "java",
+            "lua",
+            "json",
+        },
+    },
+}
