5b96b4a6d03746fdaf2c69d61f93e5667d0ff05e
Lingxi <thelastoreointhejar@gmail.com>
2023-08-11 18:40:55 +0800

    feat: Custom nvim-jdtls configuration

---
+++ ftplugin/java.lua
@@ -0,0 +1,54 @@
+-- Only configured for mac
+-- Java 17 needed for jdtls to work
+-- jdtls installed with homebrew
+
+-- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = "/Users/lingxi/" .. project_name
+
+local config = {
+    -- The command that starts the language server
+    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+    cmd = {
+
+        -- 💀
+        "java", -- or '/path/to/java17_or_newer/bin/java'
+        -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+
+        -- 💀
+        "-jar",
+        "/opt/homebrew/Cellar/jdtls/1.26.0/libexec/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar",
+
+        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+        -- Must point to the                                                     Change this to
+        -- eclipse.jdt.ls installation                                           the actual version
+
+        -- 💀
+        "-configuration",
+        "/opt/homebrew/Cellar/jdtls/1.26.0/libexec/config_mac",
+        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+        -- Must point to the                      Change to one of `linux`, `win` or `mac`
+        -- eclipse.jdt.ls installation            Depending on your system.
+
+        -- 💀
+        -- See `data directory configuration` section in the README
+        "-data",
+        workspace_dir,
+    },
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+    filetypes = { "java" },
+}
+
+require("jdtls").start_or_attach(config)
