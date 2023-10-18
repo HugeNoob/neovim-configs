d4ff0dcb5e100102ada495174a212ff5170e26a8
Lingxi Xing <thelastoreointhejar@gmail.com>
2023-10-18 14:42:57 +0800

    chore: Move to LazyVim v10

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,22 +10,23 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
-        { "LazyVim/LazyVim",                                    import = "lazyvim.plugins" },
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.linting.eslint" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
+        { import = "lazyvim.plugins.extras.lsp.none-ls" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 
         -- import/override with your plugins
         { import = "plugins" },
 
         -- disable some unwanted defaults
-        { "catppuccin/nvim",                                    name = "catppuccin",       enabled = false },
-        { "folke/tokyonight.nvim",                              enabled = false },
-        { "folke/flash.nvim",                                   enabled = false },
+        { "catppuccin/nvim", name = "catppuccin", enabled = false },
+        { "folke/tokyonight.nvim", enabled = false },
+        { "folke/flash.nvim", enabled = false },
     },
     defaults = {
         -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
